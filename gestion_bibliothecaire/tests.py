859cfc808d54695817d60c0ff8a29fa14fee96cc
JulienGeoffroy <djugeoffroy@gmail.com>
2025-04-09 17:52:05 +0200

    Ajout des modifications dans tests.py et accueil.html

--- gestion_bibliothecaire/tests.py
+++ gestion_bibliothecaire/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Media, Emprunteur, Emprunt
+from django.utils import timezone
 
-# Create your tests here.
+class MediaTestCase(TestCase):
+    def setUp(self):
+        self.media = Media.objects.create(titre="Test Livre", type="livre", disponible=True)
+
+    def test_media_creation(self):
+        self.assertEqual(self.media.titre, "Test Livre")
+        self.assertTrue(self.media.disponible)
+
+class EmpruntTestCase(TestCase):
+    def setUp(self):
+        self.emprunteur = Emprunteur.objects.create(
+            nom="Test", prenom="Utilisateur", email="test@example.com", telephone="1234567890"
+        )
+        self.media = Media.objects.create(titre="Livre Test", type="livre", disponible=True)
+        self.emprunt = Emprunt.objects.create(media=self.media, emprunteur=self.emprunteur, date_emprunt=timezone.now())
+
+    def test_emprunt_creation(self):
+        self.assertEqual(self.emprunt.media.titre, "Livre Test")
+        self.assertEqual(self.emprunt.emprunteur.nom, "Test")
+
+class PagesAccessibilityTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_homepage_access(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_connexion_page_access(self):
+        response = self.client.get(reverse("connexion"))
+        self.assertEqual(response.status_code, 200)
